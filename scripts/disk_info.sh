d39611ca49a95d0c60753ade0d4b71c8bf8441cc
Ginakira <ginakira@foxmail.com>
2020-04-21 12:20:55 +0800

    awk

--- scripts/disk_info.sh
+++ scripts/disk_info.sh
@@ -2,5 +2,5 @@
 time_info=$(date +%Y-%m-%d__%H:%M:%S)
 echo $time_info
 
-disk_info=$(df -h | tail -n +2 | tr -s '')
+disk_info=$(df -h | tail -n +2 | awk '{print $time_info" "$6" "$2" "$4" "$5}')
 echo $disk_info
