aeeb03e6de429c7b9d99783e2cbe0fa5718ce56e
Ginakira <ginakira@foxmail.com>
2020-04-24 12:39:23 +0800

    disk_info update

--- scripts/disk_info.sh
+++ scripts/disk_info.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
-time_info=$(date +%Y-%m-%d__%H:%M:%S)
-echo $time_info
 
-disk_info=$(df -h | tail -n +2 | awk '{print $6" "$2" "$4" "$5}')
-echo $disk_info
+function print_data() {
+    line=$1
+    type=$2 # 1-Partition 0-Disk
+    time_info=$3
+
+    if [[ ${type} == 1 ]]; then
+        name=$(echo $line | awk '{print $6}')
+    else
+        name="disk"
+    fi
+
+    size=$(echo $line | awk '{print $2}')
+    available=$(echo $line | awk '{print $4}')
+    use_percent=$(echo $line | awk '{print $5}')
+
+    printf "%s %d %s %s %s %s\n" $time_info $type $name $size $available $use_percent
+}
+
+time_info=$(date +%Y-%m-%d__%H:%M:%S)
+
+rows=$(df -m | awk 'END{print NR}')
+
+for ((i = 2; i <= $rows; ++i)); do
+    line=$(df -m | sed -n "${i}p")
+    #echo $line
+    print_data "$line" 1 "$time_info"
+done
+
+total_line=$(df -m --total | tail -n 1)
+print_data "$total_line" 0 "$time_info"
