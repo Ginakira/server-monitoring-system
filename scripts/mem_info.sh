62c9ee2d500d3ee18b6f87ae568c91ea0d3faf5d
Ginakira <ginakira@foxmail.com>
2020-04-25 13:30:21 +0800

    active useage

--- scripts/mem_info.sh
+++ scripts/mem_info.sh
@@ -14,6 +14,6 @@ mem_free=$(free -m | head -n 2 | tail -n 1 | awk '{print $4}')
 use_per=$(echo "scale=1;($mem_total-$mem_free)/$mem_total*100" | bc)
 
 # 占用百分比动态平均值
-active_per=$(echo "scale=1;(0.3*$1 + 0.7*$use_per) * 100" | bc)
+active_per=$(echo "scale=1;0.3*$1 + 0.7*$use_per" | bc)
 
 echo $time_info' '$mem_total'M '$mem_free'M '$use_per'% '$active_per'%'
