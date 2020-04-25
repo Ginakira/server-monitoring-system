b6017199eb5ca7928251448cdd1fa56ab6a6dc91
Ginakira <ginakira@foxmail.com>
2020-04-25 15:07:18 +0800

    cpuinfo

--- scripts/cpu_info.sh
+++ scripts/cpu_info.sh
@@ -5,9 +5,21 @@
 time_info=$(date +%Y-%m-%d__%H:%M:%S)
 
 # 平均负载
-load_ave=$(cat /proc/loadavg | awk '{printf "%s %s %s\n",$1,$2,$3}')
+load_ave=$(cut -d ' ' -f 1-3 /proc/loadavg)
 
 # CPU温度
-__temp=$(cat /sys/class/thermal/thermal_zone0/temp)
-temp=$(echo $(awk 'BEGIN{printf "%.2f\n",('$__temp'/1000)}'))
+cpu_temp=$(cat /sys/class/thermal/thermal_zone0/temp)
+cpu_temp=$(echo "scale=2;$cpu_temp/1000" | bc)
 
+# CPU占用率
+eval $(head -n 1 /proc/stat | awk -v sum1=0 '{for (i=2;i<=11;i++){sum1+=$i}
+printf("sum1=%d;idle1=%d", sum1, $5)}')
+
+sleep 0.5
+
+eval $(head -n 1 /proc/stat | awk -v sum2=0 '{for (i=2;i<=11;i++){sum2+=$i}
+printf("sum2=%d;idle2=%d", sum2, $5)}')
+
+cpu_used_per=$(echo "scale=4;(1-(${idle2}-${idle1})/(${sum2}-${sum1}))*100" | bc)
+
+echo $cpu_used_per
