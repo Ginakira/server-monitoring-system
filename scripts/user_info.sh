d0311847828bfa346debb47fc8cb79fc2c218e87
Ginakira <ginakira@foxmail.com>
2020-04-25 20:21:22 +0800

    user_info v2.0

--- scripts/user_info.sh
+++ scripts/user_info.sh
@@ -4,48 +4,32 @@
 
 time_info=$(date +%Y-%m-%d__%H:%M:%S)
 
-# 非系统用户总数 uid>=500才为普通非系统用户
-user_total=$(awk -F ":" '$3>=500{print $1}' /etc/passwd | wc -l)
+eval $(awk -F: -v sum=0 '{if ($3>=1000 && $3!=65534) {sum++; printf("all["sum"]=%s;", $1)}}\
+END {printf("user_sum=%d\n", sum)}' /etc/passwd)
 
-# 近期活跃用户列表 过滤空行 去重
-last_users=$(last | cut -d ' ' -f 1 | grep -v "^$" | sort | uniq)
+most_active_user=$(last -w | cut -d " " -f 1 | grep -v wtmp | grep -v reboot | grep -v "^$" | sort | uniq -c | sort -k 1 -n -r | awk -v num=3 '{if(num>0) {printf(",%s", $2); num--;}}' | cut -c 2-)
 
-# 逗号分割的root用户列表 带中括号
-root_users="["$(cat /etc/group | grep "root" | cut -d ':' -f 4)"]"
-
-# 当前登录用户 原始数据
-active_users=$(who)
-
-echo -n "$time_info $user_total "
-
-# 输出近期活跃用户列表
-last_count=$(echo $last_users | wc -w)
-echo -n "["
-for ((i = 1; i <= $last_count; i++)); do
-    if [ ${i} -gt 1 ]; then
-        echo -n ','
+# uid 1000 has root
+eval $(awk -F: '{if ($3==1000) printf("user_1000=%s", $1)}' /etc/passwd)
+user_with_root=${user_1000}
+users=$(cat /etc/group | grep sudo | cut -d ':' -f 4 | tr ',' ' ')
+for i in ${users}; do
+    if [[ $i == ${user_1000} ]]; then
+        continue
     fi
-
-    user=$(echo $last_users | cut -d ' ' -f ${i})
-    echo -n $user
-done
-echo -n "] "
-
-echo -n "${root_users} "
-
-# 在线用户_ip_tty
-echo -n "["
-active_count=$(echo $active_users | wc -l)
-for ((i = 1; i <= $active_count; ++i)); do
-    if [ ${i} -gt 1 ]; then
-        echo -n ','
-    fi
-
-    line=$(echo $active_users | sed -n "${i}p")
-    username=$(echo $line | awk '{print $1}')
-    ipadr=$(echo $line | awk '{print $5}' | tr -d '()')
-    tty=$(echo $line | awk '{print $2}')
-    echo -n $username'_'$ipadr'_'$tty
+    user_with_root="${user_with_root},$i"
 done
 
-echo "]"
+# if sudoers is readable
+if [[ -r /etc/sudoers ]]; then
+    for i in ${all[*]}; do
+        grep -q -w "^${i}" /etc/sudoers
+        if [[ $? -eq 0 ]]; then
+            user_with_root="${user_with_root},$i"
+        fi
+    done
+fi
+
+user_logged_in=$(w -h | awk '{printf(",%s_%s_%s", $1, $3, $2)}' | cut -c 2-)
+
+echo "${time_info} ${user_sum} [${most_active_user}] [${user_with_root}] [${user_logged_in}]"
