c43c7bd42a71aec8812700b0ff8a072fe54fc040
Ginakira <ginakira@foxmail.com>
2020-05-02 15:20:19 +0800

    malicious_process ver2.0

--- scripts/malicious_process.sh
+++ scripts/malicious_process.sh
@@ -2,32 +2,41 @@
 
 # 高占用进程检测
 
+# 第一次获取可能的高占用进程
+eval $(ps -aux -h --sort=-%cpu | awk -v num=0 \
+    '{ if($3 < 50) {exit;} else {num++; printf("cpupid["num"]=%d\n", $2);} } END {printf("cpunum=%d\n", num)}')
+
+eval $(ps -aux -h --sort=-%mem | awk -v num=0 \
+    '{ if($4 < 50) {exit;} else {num++; printf("mempid["num"]=%d\n", $2);} } END {printf("memnum=%d\n", num)}')
+
+#如果无高占用程序，则退出，否则等待5秒进入下一次检测
+if [[ ${cpunum} -gt 0 || ${memnum} -gt 0 ]]; then
+    sleep 5
+else
+    exit 0
+fi
+
 time_info=$(date +%Y-%m-%d__%H:%M:%S)
 
-# 第一次获取可能的高占用进程
-eval $(ps aux --sort=+%cpu | tail -n +2 | awk -v cnt=1 '{if($3>50){printf("first_cpu[%d]=%s;first_cmd[%d]=%s;\n", cnt, $3, cnt, $11);cnt++;}}')
-
-#如果无高占用程序，则退出
-if [[ ${#first_cpu[*]} -eq 0 ]]; then
-    echo "No high-occupancy processes"
-    exit 1
-fi
-
-sleep 5
-
-# 第二次获取可能的高占用进程
-eval $(ps aux --sort=+%cpu | tail -n +2 | awk -v cnt=1 '{if($3>50){printf("user[%d]=%s;pid[%d]=%s;cpu[%d]=%s;mem[%d]=%s;cmd[%d]=%s;\n", cnt, $1, cnt, $2, cnt, $3, cnt, $4, cnt, $11);cnt++;}}')
-
-#如果无高占用程序，则退出
-if [[ ${#cpu[*]} -eq 0 ]]; then
-    echo "No high-occupancy processes"
-    exit 1
-fi
-
-for old_proc in ${first_cmd[*]}; do
-    for ((i = 0; i <= ${#cmd[*]}; ++i)); do
-        if [[ ${cmd[$i]} == $old_proc ]]; then
-            echo "$time_info ${cmd[$i]} ${pid[$i]} ${user[$i]} ${cpu[$i]}% ${mem[$i]}%"
-        fi
+# 第二次获取可能的高CPU占用进程
+cnt=0
+if [[ ${cpunum} -gt 0 ]]; then
+    for i in ${cpupid[*]}; do
+        eval $(ps -aux -h -q $i | awk -v num=${cnt} \
+            '{ if ($3 < 50) {exit;} else {printf("Pname["num"]=%s;Pid["num"]=%d;User["num"]=%s;CpuP["num"]=%.2f;MemP["num"]=%.2f", $11, $2, $1, $3, $4)} }')
+        cnt=$((cnt + 1))
     done
+fi
+
+# 第二次获取可能的高内存占用进程
+if [[ ${memnum} -gt 0 ]]; then
+    for i in ${mempid[*]}; do
+        eval $(ps -aux -h -q $i | awk -v num=${cnt} \
+            '{ if ($4 < 50) {exit;} else {printf("Pname["num"]=%s;Pid["num"]=%d;User["num"]=%s;CpuP["num"]=%.2f;MemP["num"]=%.2f", $11, $2, $1, $3, $4)} }')
+        cnt=$((cnt + 1))
+    done
+fi
+
+for ((i = 0; i < ${cnt}; ++i)); do
+    echo "${time_info} ${Pname[$i]} ${Pid[$i]} ${User[$i]} ${CpuP[$i]}% ${MemP[$i]}%"
 done
